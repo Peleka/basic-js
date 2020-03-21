1ae6792db8d9c31681e316fad9fa467964d7c164
Elena Peletskaya <52198363+Peleka@users.noreply.github.com>
2020-03-21 17:21:10 +0300

    fix: сorrect mistake in condition

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,4 @@
-module.exports = function countCats(/* matrix */) {
+module.exports = function countCats(matrix) {
   let count = 0;
 	for (let i = 0; i < matrix.length; i++) {
 		for (let j = 0; j < matrix[i].length; j++) {
