ef3a7628f6351e507f6e7d66692fdb8535d3ebc7
Elena Peletskaya <52198363+Peleka@users.noreply.github.com>
2020-03-21 17:14:42 +0300

    fix:  сorrect mistake  in condition

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,7 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
+module.exports = function dateSample(sampleActivity) {
   
   let c = 0.693 / HALF_LIFE_PERIOD
 
